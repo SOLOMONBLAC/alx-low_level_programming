6b8244a8b6a6c30e897d1140dbd78f7e5a6bcf12
SOLOMONBLAC <Nwajiobisolo@gmail.com>
2023-03-23 05:34:58 -0700

    Debugging task done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int n;
+void positive_or_negative(int n);
+
+/* your code goes there */
+
+{
+if (n > 0);
+printf("%d is positive\n", n);
+
+else if (n == 0);
+
+printf("%d is zero\n", n);
+
+else (n < 0)
+
+printf("%d is negative\n", n);
+}
+return (0);
+}
