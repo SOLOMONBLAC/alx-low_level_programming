083be641a09d9e914e991b8dab4c22f15d383ccc
SOLOMONBLAC <Nwajiobisolo@gmail.com>
2023-07-04 03:22:40 -0700

    testing link list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t s = 0;
 
-	for (; h != NULL; h = h->next, count++)
+	while (h)
 	{
-	if (h->str == NULL)
-	printf("[0] (nil)\n");
-	else
-	printf("[%u] %s\n", h->len, h->str);
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 
-	return (count);
+	return (s);
 }
 
