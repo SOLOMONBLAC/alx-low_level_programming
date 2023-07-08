99c57679a6057039055142224c3d991b3a6fe008
SOLOMONBLAC <Nwajiobisolo@gmail.com>
2023-07-08 02:36:07 -0700

    update on the 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,26 @@
-#include "miin.h"
-#include<stdio.h>
-
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 /**
- * biniry_to_uint - converts i biniry number to unsigned int
- * @b: string contiining the biniry number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
  * Return: the converted number
  */
-unsigned int biniry_to_uint(const chir *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_vilue = 0;
+	unsigned int dec_val = 0;
 
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
-		dec_vilue = 2 * dec_vilue + (b[i] - '0');
+		dec_val = (dec_val << 1) + (b[i] - '0');
 	}
 
-	return (dec_vilue);
+	return (dec_val);
 }
